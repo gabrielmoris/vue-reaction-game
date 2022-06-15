53059cdd183ba100c593a0fc8c5720bd13c745eb
Gabriel C Moris <gabrieltrompeta@gmail.com>
2022-06-15 20:54:31 +0200

    start block component and implement logic for timeout

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,36 @@
 <template>
-  <h1>Wild west clickout</h1>
+    <h1>Wild West Clickout</h1>
+    <button @click="start" :disabled="isPlaying">Play</button>
+    <Block v-if="isPlaying" :delay="delay" />
 </template>
 
 <script>
-
-
+import Block from "./components/Block.vue";
 export default {
-  name: 'App',
-  components: {
-    
-  }
-}
+    name: "App",
+    components: { Block },
+    data() {
+        return {
+            isPlaying: false,
+            delay: null,
+        };
+    },
+    methods: {
+        start() {
+            this.delay = 3000 + Math.random() * 10000;
+            this.isPlaying = true;
+        },
+    },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="block" v-if="showBlock">
+        <img
+            src="https://media0.giphy.com/media/1gSpe0DA87PZs3Jcwt/giphy.gif?cid=790b7611c073793c535fa72957ea83cfcb8474adb1ccc45f&rid=giphy.gif&ct=g"
+            alt="west-shooting"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["delay"],
+    data() {
+        return {
+            showBlock: false,
+        };
+    },
+    mounted() {
+        setTimeout(() => {
+            this.showBlock = true;
+        }, this.delay);
+    },
+};
+</script>
+
+<style>
+.block {
+    width: 450px;
+    border-radius: 20px;
+    background: #ecd907;
+    color: white;
+    text-align: center;
+    /* padding: 100px 0;
+    margin: 40px auto; */
+}
+img {
+    width: 400px;
+}
+</style>
